a0bfcdca73522d2d335b3d0d6ef8b87f85f7e1b8
SHADOW-1104 <ing.colin@outlook.com>
2024-07-27 20:55:36 -0600

    Se agrego boton de reinicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import { useState } from "react";
 
 export const App = () => {
-  const urlBase = 'https://randomuser.me/api';
-  const [usuarios, setUsuarios] = useState([]);
+  const urlBase = 'https://randomuser.me/api'
+  const [usuarios, setUsuarios] = useState([])
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchUsuarios();
-  };
+    e.preventDefault()
+    fetchUsuarios()
+  }
 
   const fetchUsuarios = async () => {
     try {
-      const response = await fetch(urlBase);
-      const data = await response.json();
-      setUsuarios(data.results); // Asegúrate de obtener la propiedad 'results'
+      const response = await fetch(urlBase)
+      const data = await response.json()
+      setUsuarios(data.results)// Asegúrate de obtener la propiedad 'results'
     } catch (error) {
-      console.error('Ocurrió un error', error);
+      console.error('Ocurrió un error', error)
     }
-  };
+  }
+
+  const reset = () => {
+    setUsuarios([])
+  }
 
   return (
     <>
-    <nav>
-      <h1>Generador de Personas Aleatorios</h1>
-    </nav>
+      <nav>
+        <h1>Generador de Personas Aleatorios</h1>
+      </nav>
 
       {usuarios.map((usuario, index) => (
         <div className="container">
 
           <div key={index} className="infoCard">
             <div className="imgContent">
-            <img src={usuario.picture.large} alt="User thumbnail" />
+              <img src={usuario.picture.large} alt="User thumbnail" />
             </div>
             <h2>Nombre: {usuario.name.first} {usuario.name.last}</h2>
             <h4>Edad: {usuario.dob.age} años</h4>
@@ -43,6 +47,7 @@ export const App = () => {
       ))}
 
       <button className="btn" type="button" onClick={handleSubmit}>Crear</button>
+      <button className="btn" type="button" onClick={reset}>Limpiar</button>
 
     </>
   );
